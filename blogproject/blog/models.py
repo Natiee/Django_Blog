7cad252fe26ed5656a6866fb4ff3f5a8f38e2b52
Natiee <2471374200@qq.com>
2018-09-18 13:28:08 +0800

    配置数据库字段

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    """
+    分类
+    Django模型必须继承models.Model类
+    CharField,max_length=100
+    """
+    name = models.CharField(max_length=100)
+
+
+class Tag(models.Model):
+    """
+    标签
+    CharField,max_length=100
+    """
+    name = models.CharField(max_length=100)
+
+
+class Post(models.Model):
+    """
+    文章标题;
+    文章正文;
+    创建时间;
+    最后修改时间;
+    文章摘要;
+    作者
+    """
+    # 标题
+    title = models.CharField(max_length=70)
+
+    # 正文,使用TextField
+    body = models.TextField()
+
+    # 创建时间和最后修改时间,使用DateTiemField
+    create_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+
+    # 文章摘要 可以为空,blank=True
+    excerpt = models.CharField(max_length=200,blank=True)
+
+    category = models.ForeignKey(Category)  # 一对多关系,一篇文章只能对一个分类
+    tags = models.ManyToManyField(Tag,blank=True)  # 多对多关系,一个文章可以有多个标签,一个标签有多个文章
+
+    # 作者,使用原生的user
+    author = models.ForeignKey(User)
+
--- blogproject/blogproject/__init__.py
+++ blogproject/blogproject/__init__.py
@@ -1,1 +1,2 @@
 import pymysql
+pymysql.install_as_MySQLdb()
