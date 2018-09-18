fe68bd06a3db8544a708934afe97dc20d5b75163
Natiee <2471374200@qq.com>
2018-09-18 22:30:23 +0800

    增加templates文件夹,blog首页测试

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+
+    return render(request,'blog/index.html',context={
+        'title':'我的博客首页',
+        'welcome':'欢迎访问我的博客首页'
+    })
