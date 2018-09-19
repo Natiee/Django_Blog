18bc24270fdd8d1a85c5dfc6cec7fe0c9cc4c2a0
2471374200@qq.com <2471374200@qq.com>
2018-09-19 19:55:40 +0800

    添加Markdown语法和代码高亮

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,7 +1,9 @@
+import markdown
 from django.shortcuts import render,get_object_or_404
 from .models import Post
 # Create your views here.
 from django.http import HttpResponse
+from markdown.extensions.toc import TocExtension
 
 
 def index(request):
@@ -12,4 +14,12 @@ def index(request):
 def detail(request, pk):
     # 如果pk对应的文章不存在,就返回404错误
     post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(
+        post.body,
+        extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',  # 语法高亮扩展
+            'markdown.extensions.toc',  # 自动生成目录
+        ]
+    )
     return render(request,'blog/detail.html',context={'post':post})
