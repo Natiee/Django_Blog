55f351729ca983f8f7ed86036dd4891dfca892c7
2471374200@qq.com <2471374200@qq.com>
2018-09-24 12:06:19 +0800

    实现显示分类下文章数量的功能

--- blogproject/blog/templatetags/blog_tags.py
+++ blogproject/blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from ..models import Post,Category
 from django import template
+from django.db.models import Count
 
 # 实例化template.Library类,
 register = template.Library()
@@ -20,7 +21,7 @@ def archives():
 @register.simple_tag
 def get_categories():
     """分类模板标签"""
-    return Category.objects.all()
+    return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
 
 
 
