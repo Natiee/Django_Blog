8468529281f29fd6283430bfa7a4a4c857e7ec1c
2471374200@qq.com <2471374200@qq.com>
2018-09-27 17:21:54 +0800

    实现简单全站搜索功能

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -5,6 +5,7 @@ from comments.forms import CommentForm
 from django.views.generic import ListView,DetailView
 from django.utils.text import slugify
 from markdown.extensions.toc import TocExtension
+from django.db.models import Q
 # Create your views here.
 
 class IndexView(ListView):
@@ -233,3 +234,15 @@ class TagView(IndexView):
     def get_queryset(self):
         tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
         return super(TagView,self).get_queryset().filter(tags=tag)
+
+def search(request):
+    q = request.GET.get('q')
+    error_msg = ''
+
+    if not q:
+        error_msg = '请输入关键词'
+        return render(request, 'blog/index.html', {'error_msg': error_msg})
+
+    post_list = Post.objects.filter(Q(title__icontains=q) | Q(body__icontains=q))
+    return render(request, 'blog/index.html', {'error_msg': error_msg,
+                                               'post_list': post_list})
